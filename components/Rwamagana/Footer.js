15817e361b0267c108d553253dddf76d1108ab1f
Samuel Xanda <samuelxanda752@gmail.com>
2024-10-16 22:59:36 +0200

    Create Footer.js

---
+++ components/Rwamagana/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="p-6 pt-4 text-2xl bg-pink neuebit">
+      <div className="text-center">
+        <p>Built  by Samuel Xanda, for teenagers</p>
+      </div>
+    </div>
+  );
+}
