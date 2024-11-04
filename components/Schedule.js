62f316bdc02c4a85ef0d404a2289f43a8f879f3f
paramveersi <121735202+pmvrsi@users.noreply.github.com>
2024-11-04 04:46:13 +0000

    Update Schedule.js
    Schedule for Counterspell Wolverhampton

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,18 +1,16 @@
 export default function Schedule() {
   const schedule = [
-    { time: "11:00 AM", event: "Doors open" },
-    { time: "12:00 PM", event: "Opening ceremony" },
-    { time: "12:30 PM", event: "Lunch" },
-    { time: "1:00 PM", event: "Start working on your game!" },
-    { time: "2:00 PM", event: "Gamedev workshop 1" },
-    { time: "4:00 PM", event: "Activity 1" },
-    { time: "4:00 PM", event: "Gamedev workshop 2" },
-    { time: "6:00 PM", event: "Dinner" },
-    { time: "8:00 PM", event: "Lightning talks" },
-    { time: "12:00 AM", event: "Midnight surprise" },
-    { time: "8:00 AM", event: "Breakfast" },
-    { time: "10:30 AM", event: "Game demos!" },
-    { time: "12:00 PM", event: "Closing ceremony" },
+    { time: "9:30 AM", event: "Doors open" },
+    { time: "10:00 AM", event: "Opening ceremony" },
+    { time: "11:00 AM", event: "Start working on your game!" },
+    { time: "12:00 AM", event: "Lunch" },
+    { time: "1:00 PM", event: "Gamedev workshop 1" },
+    { time: "2:00 PM", event: "Activity 1" },
+    { time: "3:00 PM", event: "Gamedev workshop 2" },
+    { time: "4:00 PM", event: "Lighting Talks" },
+    { time: "5:30 PM", event: "Dinner" },
+    { time: "7:00 PM", event: "Game Demos"}, 
+    { time: "8:00 PM", event: "Closing Ceremony"},
   ];
 
   return (
