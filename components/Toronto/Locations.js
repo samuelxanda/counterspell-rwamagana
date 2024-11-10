5ea09779947fe42a8c8628f13e3ae839dee75979
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-11-09 22:08:35 -0500

    Add map, and some minor adjustments

--- components/Toronto/Locations.js
+++ components/Toronto/Locations.js
@@ -5,7 +5,18 @@ const Map = dynamic(() => import("./Map"), { ssr:false });
 
 export default function Locations() {
   return (
-    <div className="relative px-6 py-24 text-center neuebit bg-darker retro">
+    
+      <div className="relative px-6 py-24 text-center text-5xl neuebit bg-darker retro">
+          Meet Us At{' '} 
+          <a 
+            href="https://www.google.com/maps/dir/?api=1&destination=620+King+St+W,+Toronto,+ON+M5V+1M7"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-300"
+          >
+            Shopify Toronto 620 King St W
+          </a>!
+
       <div className="flex justify-center text-center text-2xl space-y-2 my-20">
         <div className="border-4 border-dashed border-pink py-6 px-6 neuebit tracking-wider text-4xl max-w-5xl w-full">
           <Map full={false} />
@@ -16,7 +27,7 @@ export default function Locations() {
         <div className="px-6 py-6 text-4xl tracking-wider border-4 border-dashed border-pink neuebit">
           <p>Counterspell is organized by teenagers, for teenagers.</p>
           <p>
-            See a city near you on the map? {" "}
+            Want to join click the link! {" "}
             <a
               href="https://forms.hackclub.com/t/sdMpoL7wK9us"
               target="_blank"
